d7774c90dbcb14d232a05d6aa383e4f5c9a3448d
Yigit Demirbas <yigit.demirbas@gmail.com>
2023-05-27 11:29:01 +0200

    change theme for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,71 +1,71 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.color_scheme = 'rose-pine'
+config.color_scheme = "Catppuccin Mocha"
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.font_size = 16
 
 -- timeout_milliseconds defaults to 1000 and can be omitted
-config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-  { 
-    key = ".", 
-    mods = "LEADER", 
-    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }), 
-  },
-  {
-    key = "-",
-    mods = "LEADER",
-    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "h",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Left"),
-  },
-  {
-    key = "j",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Down"),
-  },
-  {
-    key = "k",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Up"),
-  },
-  {
-    key = "l",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Right"),
-  },
-  {
-    key = "a",
-    mods = "LEADER|CTRL",
-    action = wezterm.action.SendString("\x01"),
-  },
-  { 
-    key = 'LeftArrow', 
-    mods = 'OPT', 
-    action = wezterm.action.SendString("\x1bb"),
-  },
-  { 
-    key = 'RightArrow', 
-    mods = 'OPT', 
-    action = wezterm.action.SendString("\x1bf"), 
-  },
-  {
-    key = 'w',
-    mods = 'LEADER',
-    action = wezterm.action.CloseCurrentPane { confirm = true },
-  },
+	{
+		key = ".",
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "h",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendString("\x01"),
+	},
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = wezterm.action.SendString("\x1bb"),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = wezterm.action.SendString("\x1bf"),
+	},
+	{
+		key = "w",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
 }
 
 return config
