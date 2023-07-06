ce358430c7d5c04ea0c4a804075eb8c26f7c102f
Yigit Demirbas <yigit.demirbas@gmail.com>
2023-07-06 21:50:52 +0200

    add hcl as ensured lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -129,7 +129,7 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 -- Treesitter config
 require("nvim-treesitter.configs").setup({
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go" },
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "hcl" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
