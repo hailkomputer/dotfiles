2342dba98e1f1be8d4999132e3d46b00c4c2c338
Yigit Demirbas <yigit.demirbas@gmail.com>
2023-08-03 22:56:56 +0200

    remove netrw related configurations

--- nvim/init.lua
+++ nvim/init.lua
@@ -141,13 +141,9 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
 
 -- Remaps
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
