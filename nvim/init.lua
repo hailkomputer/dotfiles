2178113c0df517f1a7b8980f3fb49b5ed78cd190
Yigit Demirbas <yigit.demirbas@gmail.com>
2024-01-15 15:59:52 +0100

    gofumpt

--- nvim/init.lua
+++ nvim/init.lua
@@ -320,7 +320,8 @@ lsp.setup()
 local null_ls = require("null-ls")
 
 local sources = {
-	null_ls.builtins.formatting.goimports,
+	null_ls.builtins.formatting.gofumpt,
+	null_ls.builtins.formatting.goimports_reviser,
 	null_ls.builtins.formatting.stylua,
 	null_ls.builtins.formatting.rustfmt,
 	null_ls.builtins.formatting.terraform_fmt.with({
