d96c6b299a446d46149bf5b895da342391b9fc57
Yigit Demirbas <yigit.demirbas@gmail.com>
2024-06-02 22:52:28 +0200

    add dap debugger

--- nvim/init.lua
+++ nvim/init.lua
@@ -152,6 +152,11 @@ require("lazy").setup({
 				-- },
 			})
 
+			-- diagnostics
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+			vim.keymap.set("n", "<leader>[d", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "<leader>]d", vim.diagnostic.goto_next)
+
 			local capabilities = nil
 			if pcall(require, "cmp_nvim_lsp") then
 				capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -311,6 +316,54 @@ require("lazy").setup({
 			}
 		end,
 	},
+	-- debugger
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"leoluz/nvim-dap-go",
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+			"nvim-neotest/nvim-nio",
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			local dap = require("dap")
+			local ui = require("dapui")
+
+			require("dapui").setup()
+			require("dap-go").setup()
+			require("nvim-dap-virtual-text").setup()
+			vim.fn.sign_define(
+				"DapBreakpoint",
+				{ text = "🔴", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+			)
+			vim.keymap.set("n", "<space>db", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<space>rc", dap.run_to_cursor)
+			vim.keymap.set("n", "<space>?", function()
+				require("dapui").eval(nil, { enter = true })
+			end)
+
+			vim.keymap.set("n", "<F1>", dap.continue)
+			vim.keymap.set("n", "<F2>", dap.step_into)
+			vim.keymap.set("n", "<F3>", dap.step_over)
+			vim.keymap.set("n", "<F4>", dap.step_out)
+			vim.keymap.set("n", "<F5>", dap.step_back)
+			vim.keymap.set("n", "<F6>", dap.restart)
+
+			dap.listeners.before.attach.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				ui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				ui.close()
+			end
+		end,
+	},
 })
 
 -- Settings
@@ -357,12 +410,6 @@ vim.keymap.set("n", "<CR>", function()
 		return vim.keycode("<CR>")
 	end
 end, { expr = true })
-
--- diagnostics
-vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float)
-vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<leader>ds", vim.diagnostic.setqflist)
 
 vim.keymap.set("n", "<M-,>", "<c-w>5<")
 vim.keymap.set("n", "<M-.>", "<c-w>5>")
