a4af72c27d3af4045818da9218984f5c4829e1a2
Yigit Demirbas <yigit.demirbas@gmail.com>
2024-06-02 02:20:41 +0200

    tmux conf changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,17 +9,9 @@ if wezterm.config_builder then
 end
 
 config.color_scheme = "Catppuccin Mocha"
-config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = true
+config.enable_tab_bar = false
 config.font_size = 16
-config.enable_scroll_bar = false
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
-}
-
+config.window_decorations = "RESIZE"
 config.mouse_bindings = {
 	{
 		event = { Up = { streak = 1, button = "Left" } },
