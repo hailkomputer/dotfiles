f54f42e9dcdad755cb67817fa75f68e517a310ab
Yigit Demirbas <yigit.demirbas@gmail.com>
2024-08-30 21:02:37 +0200

    remove debugger

--- nvim/init.lua
+++ nvim/init.lua
@@ -316,54 +316,6 @@ require("lazy").setup({
 			}
 		end,
 	},
-	-- debugger
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"leoluz/nvim-dap-go",
-			"rcarriga/nvim-dap-ui",
-			"theHamsta/nvim-dap-virtual-text",
-			"nvim-neotest/nvim-nio",
-			"williamboman/mason.nvim",
-		},
-		config = function()
-			local dap = require("dap")
-			local ui = require("dapui")
-
-			require("dapui").setup()
-			require("dap-go").setup()
-			require("nvim-dap-virtual-text").setup()
-			vim.fn.sign_define(
-				"DapBreakpoint",
-				{ text = "🔴", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-			)
-			vim.keymap.set("n", "<space>db", dap.toggle_breakpoint)
-			vim.keymap.set("n", "<space>rc", dap.run_to_cursor)
-			vim.keymap.set("n", "<space>?", function()
-				require("dapui").eval(nil, { enter = true })
-			end)
-
-			vim.keymap.set("n", "<F1>", dap.continue)
-			vim.keymap.set("n", "<F2>", dap.step_into)
-			vim.keymap.set("n", "<F3>", dap.step_over)
-			vim.keymap.set("n", "<F4>", dap.step_out)
-			vim.keymap.set("n", "<F5>", dap.step_back)
-			vim.keymap.set("n", "<F6>", dap.restart)
-
-			dap.listeners.before.attach.dapui_config = function()
-				ui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				ui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				ui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				ui.close()
-			end
-		end,
-	},
 })
 
 -- Settings
