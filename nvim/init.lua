fd9ba6c39a2ed8d056bd5e4b8899bf80b455c195
Yigit Demirbas <yigit.demirbas@gmail.com>
2024-11-19 08:04:23 +0000

    added openfga plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -316,6 +316,19 @@ require("lazy").setup({
 			}
 		end,
 	},
+	{
+		"hedengran/fga.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig", -- Optional, for LSP integration
+			"nvim-treesitter/nvim-treesitter", -- Optional, for enhanced syntax highlighting
+		},
+		config = function()
+			require("fga").setup({
+				install_treesitter_grammar = true,
+				lsp_server = "/path/to/vscode-ext/server/out/server.node.js",
+			})
+		end,
+	},
 })
 
 -- Settings
